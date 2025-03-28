0d0d635236b1bacd76d3e106cfb9941933f69d20
loristeffel <lrsteffel10@gmail.com>
2025-03-27 17:12:53 -0700

    Add files via upload

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+
+// Firebase Configuration
+export const firebaseConfig = {
+  "apiKey": "AIzaSyDuQchhEVnrGJ2ea_d54En-QtuP2KEOOSs",
+  "authDomain": "roadtojarvis-e6cf9.firebaseapp.com",
+  "projectId": "roadtojarvis-e6cf9",
+  "storageBucket": "roadtojarvis-e6cf9.firebasestorage.app",
+  "messagingSenderId": "751559112417",
+  "appId": "1:751559112417:web:259183fd9e800c3db929a0"
+};
